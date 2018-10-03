9454356c58e33fd959a74c6eff2fee0e579a1c01
Jesse Ballera <jesse.ballera@upraxis.com>
2018-10-03 17:27:31 +0800

    initial project structure

---
+++ src/test/java/com/upraxis/ConfigRegistry/ConfigRegistryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.upraxis.ConfigRegistry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigRegistryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
